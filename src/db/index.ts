6f2de7eb1149a40c0b577b1c3ffbdbd12b784be4
sgantaya <sgantaya@tibco.com>
2020-05-10 23:29:38 +0530

    resolved database connection message issue and added app.yaml file.

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,47 +2,53 @@ import Config from '../config/config';
 import Logger from '../helper/logger';
 import {connection, connect, ConnectionOptions} from 'mongoose';
 import registration from '../model/registration';
+import Utils from '../helper/utils';
 
-
+const NODE_ENV = Utils.getEnvVariable('NODE_ENV', true);
 
 export default class Db {
     public static async connect() {
-        try{
+        try {
+            connection.setMaxListeners(0);
+            connection.on('error', (err) => {
+                Logger.error('MongoDB event error: ${error}');
+            });
+
+            connection.on('connected', () => {
+                Logger.info('MongoDB event connected');
+            });
+
+            connection.on('disconnected', () => {
+                Logger.info('MongoDB event disconnected');
+            });
+
+            connection.on('reconnected', () => {
+                Logger.info('MongoDB event reconnected');
+            });
+
+            connection.on('open', () => {
+                Logger.info('Connection with database succeeded');
+            });
+            if (NODE_ENV === 'development') {
                 await connect('mongodb://' + Config.database.username + ':' + Config.database.password + '@' + Config.database.host + ':' + Config.database.port + '/' + Config.database.name + '?authSource=admin',
                     Config.databaseOptions as ConnectionOptions
                 );
-                connection.setMaxListeners(0);
-                connection.on('error', (err) => {
-                    Logger.error('MongoDB event error: ${error}');
-                });
+            } else {
+                await connect('mongodb://' + Config.database.username + ':' + Config.database.password + '@' + Config.database.host + ':' + Config.database.port + '/' + Config.database.name,
+                    Config.databaseOptions as ConnectionOptions
+                );
+            }
 
-                connection.on('connected', () => {
-                    Logger.info('MongoDB event connected');
-                });
-
-                connection.on('disconnected', () => {
-                    Logger.info('MongoDB event disconnected');
-                });
-
-                connection.on('reconnected', () => {
-                    Logger.info('MongoDB event reconnected');
-                });
-
-                connection.once('open', () => {
-                    Logger.info('Connection with database succeeded');
-                });
-
-        }
-        catch(error){
+        } catch (error) {
             Logger.error(`Error in connecting database: ${error}`);
             throw error;
         }
 
     };
 
-    public static addModals = (): void =>{
+    public static addModals = (): void => {
         Db.models.registration = registration;
     };
 
-    public static models:any ={};
+    public static models: any = {};
 };
